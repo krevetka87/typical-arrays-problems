acebbbe6226969c3260f430cae4e97f9bafa6bf2
krevetka87 <bonanast4sia@yandex.ru>
2021-02-14 11:02:05 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array = 0) {
+  return array.length ? Math.min(...array) : 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array = 0) {
+  return array.length ? Math.max(...array) : 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array = 0) {
+  return array.length ? array.reduce((a, b) => a + b, 0) / array.length : 0;
 }
